d262e7da1d0222168712c75655559cb8a39b9559
leshikkk <58438315+leshikkk@users.noreply.github.com>
2020-01-20 21:46:27 +0300

    del

--- scripts/JsPractice1.js
+++ scripts/JsPractice1.js
@@ -1,8 +1,1 @@
 
-function jsPractice1() {
-   var stringVar = "Lorem ipsum dolor sit amet.";
-   alert(stringVar);
-   stringVar = "Lorem ipsum dolor sit amet, consectetur.";
-   console.log(stringVar);
-}
-document.getElementById('jsPractice1').addEventListener('click', jsPractice1);
